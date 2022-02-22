1c17d8318b9aecd321a04e4135bf30dcf93553a3
Gautam Agrawal <gautam@Gautams-MacBook-Air.local>
2022-02-22 21:31:32 +0530

    second commit

--- src/main/java/atc/UserWebApplication.java
+++ src/main/java/atc/UserWebApplication.java
@@ -1,4 +1,4 @@
-package com.atc;
+package atc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/atc/controller/GreetingController.java
+++ src/main/java/atc/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.atc.controller;
+package atc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
