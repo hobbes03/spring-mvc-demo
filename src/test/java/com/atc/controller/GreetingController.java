a5c967a2178e982237908fdfb0ed1167d4b29e99
Gautam Agrawal <gautam@Gautams-MacBook-Air.local>
2022-02-22 21:20:48 +0530

    first commit

---
+++ src/test/java/com/atc/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.atc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="userId", required = false, defaultValue = "World") String userId, Model model) {
+        model.addAttribute("name", userId);
+        return "greeting";
+    }
+
+}
